f823e8776e9d26ebdf67753b49a0d979a919310c
laipmv-1193 <pham.mai.van.lai@sun-asterisk.com>
2021-10-14 08:36:08 +0700

    Done Chapter 9

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,14 @@
 class SessionsController < ApplicationController
-  def new; end
+  def new
+    return unless logged_in?
+
+    redirect_to root_url
+  end
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user&.authenticate(params[:session][:password])
-      log_in user
-      redirect_to user
+      user_authenticated user
     else
       flash.now[:danger] = t("custom_errors.invalid_login_msg")
       render :new
